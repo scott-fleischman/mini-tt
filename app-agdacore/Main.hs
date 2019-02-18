64b615ae00326dfad9f19ff71e4b341c66cf747d
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2019-02-18 10:40:49 -0800

    Propagate errors in types rather than use `error`.

--- app-agdacore/Main.hs
+++ app-agdacore/Main.hs
@@ -108,7 +108,7 @@ type TVal  = Val
 data Clos = Cl Patt Exp Rho | ClCmp Clos Name deriving Show
 
 -- instantiation of a closure by a value
-(*) :: Clos -> Val -> Val
+(*) :: Clos -> Val -> Either String Val
 (Cl p e rho) * v = eval e (UpVar rho p v)
 (ClCmp f c ) * v = f * Con c v
 
@@ -142,23 +142,25 @@ clCmp g c = \v -> g * (Con c v)
 type Pos = ((Int, Int), String)
 
 
-app :: Val -> Val -> Val
+app :: Val -> Val -> G Val
 app (Lam f)             v        = f * v
-app (Fun _ (ces, rho)) (Con c v) = app (eval e rho) v
-                                   where e = head [e' | (c',e') <- ces, c == c']
-app (Fun pos s)        (Nt k)    = Nt(NtFun pos s k)
-app (Nt k)             m         = Nt(App k m)
-app w u = error ("app " ++ showVal 0 w ++ showVal 0 u)
+app (Fun _ (ces, rho)) (Con c v) = do
+  evalERho <- eval e rho
+  app evalERho v
+  where e = head [e' | (c',e') <- ces, c == c']
+app (Fun pos s)        (Nt k)    = Right (Nt(NtFun pos s k))
+app (Nt k)             m         = Right (Nt(App k m))
+app w u = Left ("app " ++ showVal 0 w ++ showVal 0 u)
 
-vfst :: Val -> Val
-vfst (Pair u1 _) = u1
-vfst (Nt k)      = Nt(Fst k)
-vfst w = error ("vfst " ++ showVal 0 w)
+vfst :: Val -> G Val
+vfst (Pair u1 _) = Right u1
+vfst (Nt k)      = Right $ Nt (Fst k)
+vfst w = Left ("vfst " ++ showVal 0 w)
 
-vsnd :: Val -> Val
-vsnd (Pair _ u2) = u2
-vsnd (Nt k)      = Nt(Snd k)
-vsnd w =  error ("vsnd " ++ showVal 0 w)
+vsnd :: Val -> G Val
+vsnd (Pair _ u2) = Right u2
+vsnd (Nt k)      = Right $ Nt (Snd k)
+vsnd w = Left ("vsnd " ++ showVal 0 w)
 
 ---------------------------------------------
 -- Environment
@@ -166,14 +168,21 @@ vsnd w =  error ("vsnd " ++ showVal 0 w)
 
 data Rho = RNil | UpVar Rho Patt Val | UpDec Rho Decl deriving Show
 
-getRho :: Rho -> Name -> Val
+getRho :: Rho -> Name -> G Val
 getRho (UpVar rho p v) x | x `inPat` p = patProj p x v
                          | otherwise   = getRho rho x
-getRho (UpDec rho (Def  p _ e)) x | x `inPat` p = patProj p x (eval e rho)
-                                  | otherwise   = getRho rho x
+getRho (UpDec rho (Def  p _ e)) x
+  | x `inPat` p =
+    do
+      evalERho <- eval e rho
+      patProj p x evalERho
+  | otherwise   = getRho rho x
 getRho rho0@(UpDec rho (Drec p _ e)) x
-                                  | x `inPat` p = patProj p x (eval e rho0)
-                                  | otherwise   = getRho rho x
+  | x `inPat` p =
+    do
+      evalERho0 <- eval e rho0
+      patProj p x evalERho0
+  | otherwise   = getRho rho x
 getRho RNil _ = error "getRho"
 
 inPat :: Name -> Patt -> Bool
@@ -181,11 +190,19 @@ inPat x (PVar y)      = x == y
 inPat x (PPair p1 p2) = inPat x p1 || inPat x p2
 inPat _ Punit         = False
 
-patProj :: Patt -> Name -> Val -> Val
-patProj (PVar y)      x v | x == y       = v
-patProj (PPair p1 p2) x v | x `inPat` p1 = patProj p1 x (vfst v)
-                          | x `inPat` p2 = patProj p2 x (vsnd v)
-patProj _ _ _ = error "patProj"
+patProj :: Patt -> Name -> Val -> G Val
+patProj (PVar y)      x v
+  | x == y       = Right v
+patProj (PPair p1 p2) x v
+  | x `inPat` p1 =
+    do
+      vfstV <- vfst v
+      patProj p1 x vfstV
+  | x `inPat` p2 = 
+    do
+      vsndV <- vsnd v
+      patProj p2 x vsndV
+patProj _ _ _ = Left "patProj"
 
 lRho :: Rho -> Int
 lRho RNil            = 0
@@ -193,26 +210,49 @@ lRho (UpVar rho _ _) = lRho rho + 1
 lRho (UpDec rho _  ) = lRho rho
 
 
-eval :: Exp -> Rho -> Val
+eval :: Exp -> Rho -> G Val
 eval e0 rho = case e0 of
-    ESet          -> Set
+    ESet          -> Right (Set)
     EDec d e      -> eval e (UpDec rho d)
-    ELam p e      -> Lam $ mkCl p e rho
-    EPi  p a b    -> Pi  (eval a rho) $ mkCl p b rho
-    ESig p a b    -> Sig (eval a rho) $ mkCl p b rho
-    EOne          -> One
-    Eunit         -> Unit
-    EFst e        -> vfst (eval e rho)
-    ESnd e        -> vsnd (eval e rho)
-    EApp e1 e2    -> app (eval e1 rho) (eval e2 rho)
+    ELam p e      -> Right (Lam $ mkCl p e rho)
+    EPi  p a b    ->
+      do
+        evalERho <- eval a rho
+        Right (Pi  evalERho $ mkCl p b rho)
+    ESig p a b    ->
+      do
+        evalARho <- eval a rho
+        Right $ Sig evalARho $ mkCl p b rho
+    EOne          -> Right (One)
+    Eunit         -> Right (Unit)
+    EFst e        ->
+      do
+        evalERho <- eval e rho
+        vfst evalERho
+    ESnd e        ->
+      do
+        evalERho <- eval e rho
+        vsnd evalERho
+    EApp e1 e2    ->
+      do
+        evalE1Rho <- eval e1 rho
+        evalE2Rho <- eval e2 rho
+        app evalE1Rho evalE2Rho
     EVar x        -> getRho rho x
-    EPair e1 e2   -> Pair  (eval e1 rho) (eval e2 rho)
-    ECon c e1     -> Con c (eval e1 rho)
+    EPair e1 e2   ->
+      do
+        evalE1Rho <- eval e1 rho
+        evalE2Rho <- eval e2 rho
+        Right (Pair evalE1Rho evalE2Rho)
+    ECon c e1     ->
+      do
+        evalE1Rho <- eval e1 rho
+        Right (Con c evalE1Rho)
     EData (DataTk pos) cas
-                  -> Data pos ([(c,a) | Summand c a <- cas], rho)
+                  -> Right (Data pos ([(c,a) | Summand c a <- cas], rho))
     ECase (CaseTk pos) ces
-                  -> Fun  pos ([(c,e) | Branch  c e <- ces], rho)
-    e -> error $ "eval: should have been desugared\n e = " ++ printTree e
+                  -> Right (Fun  pos ([(c,e) | Branch  c e <- ces], rho))
+    e -> Left $ "eval: should have been desugared\n e = " ++ printTree e
 
 -------------------------------------------
 -- Readback functions
@@ -221,35 +261,79 @@ eval e0 rho = case e0 of
 eVar :: String -> Exp
 eVar = EVar . Ident
 
-rbV :: Int -> Val  -> Exp -- to do: change to Normal Expression
+rbV :: Int -> Val  -> G Exp -- to do: change to Normal Expression
 
 rbV i v0 = case v0 of
-      Lam f      -> ELam (pat i) (rbV (i+1) (f * gen i))
-      Pair u v   -> EPair (rbV i u) (rbV i v)
-      Con  c v   -> ECon  c (rbV i v)
-      Unit       -> Eunit 
-      Set        -> ESet
-      Pi  t g    -> EPi  (pat i) (rbV i t) (rbV (i+1) (g * gen i))
-      Sig t g    -> ESig (pat i) (rbV i t) (rbV (i+1) (g * gen i))
-      One        -> EOne
-      Fun  pos (_,rho) -> foldr (flip EApp) (eVar $ show pos) (rbRho i rho)
-      Data pos (_,rho) -> foldr (flip EApp) (eVar $ show pos) (rbRho i rho)
+      Lam f      ->
+        do
+          fGenI <- f * gen i
+          rbVSucI <- rbV (i+1) fGenI
+          return $ ELam (pat i) rbVSucI
+      Pair u v   ->
+        do
+          valU <- rbV i u
+          valV <- rbV i v
+          return $ EPair valU valV
+      Con  c v   ->
+        do
+          valV <- rbV i v
+          return $ ECon c valV
+      Unit       -> return Eunit
+      Set        -> return ESet
+      Pi  t g    ->
+        do
+          gGenI <- g * gen i
+          valG <- rbV (i+1) gGenI
+          valT <- rbV i t
+          return $ EPi (pat i) valT valG
+      Sig t g    ->
+        do
+          gGenI <- (g * gen i)
+          valG <- (rbV (i+1) gGenI)
+          valT <- (rbV i t)
+          return $ ESig (pat i) valT valG
+      One        -> return EOne
+      Fun  pos (_,rho) ->
+        do
+          valI <- rbRho i rho
+          return $ foldr (flip EApp) (eVar $ show pos) valI
+      Data pos (_,rho) ->
+        do
+          valI <- (rbRho i rho)
+          return $ foldr (flip EApp) (eVar $ show pos) valI
       Nt k       -> rbN i k
     where pat j = PVar $ Ident $ "G#" ++ show j
           gen j = Nt $ Gen j (Ident "G#")
 
-rbN :: Int -> Neut -> Exp
+rbN :: Int -> Neut -> G Exp
 rbN i k0 = case k0 of
-      Gen j x -> eVar $ printTree x++show j
-      App k m -> EApp (rbN i k) (rbV i m)
-      Fst k   -> EFst (rbN i k)
-      Snd k   -> ESnd (rbN i k)
+      Gen j x -> return $ eVar $ printTree x++show j
+      App k m ->
+        do
+          valK <- rbN i k
+          valM <- rbV i m
+          return $ EApp valK valM
+      Fst k   ->
+        do
+          valK <- rbN i k
+          return $ EFst valK
+      Snd k   ->
+        do
+          valK <- rbN i k
+          return $ ESnd valK
       NtFun pos (_,rho) k ->
-        foldr (flip EApp) (eVar $ show pos) (rbRho i rho) `EApp` rbN i k
+        do
+          rbI <- (rbRho i rho)
+          rbK <- rbN i k
+          return $ foldr (flip EApp) (eVar $ show pos) rbI `EApp` rbK
 
-rbRho :: Int -> Rho -> [Exp]
-rbRho _ RNil = []
-rbRho i (UpVar rho _ v) = rbV i v : rbRho i rho
+rbRho :: Int -> Rho -> G [Exp]
+rbRho _ RNil = return []
+rbRho i (UpVar rho _ v) =
+  do
+    r1 <- rbV i v
+    rs <- rbRho i rho
+    return $ r1 : rs
 rbRho i (UpDec rho _  ) = rbRho i rho
 
 ------------------------------------------------
@@ -273,8 +357,13 @@ lookupG x xts = case lookup x xts of Just t  -> return t
 upG :: Gamma -> Patt -> TVal -> Val -> G Gamma
 upG gma Punit         _         _ = return gma
 upG gma (PVar x)      t         _ = return $ (x,t):gma
-upG gma (PPair p1 p2) (Sig t g) v = do gma1 <- upG gma p1 t (vfst v)
-                                       upG gma1 p2 (g * vfst v) (vsnd v)
+upG gma (PPair p1 p2) (Sig t g) v =
+  do
+    vfstV <- (vfst v)
+    gma1 <- upG gma p1 t vfstV
+    vsndV <- (vsnd v)
+    gVfstV <- (g * vfstV)
+    upG gma1 p2 gVfstV vsndV
 upG _   p             _         _ = fail $ "upG: p = " ++ printTree p
 
 -------------------------------------------------
@@ -292,27 +381,31 @@ checkI :: Rho -> Gamma -> Exp  -> G TVal
 checkD rho gma d@(Def  p a e) = do
   debug $ "checking "++ printTree d
   checkT rho gma a
-  let t = eval a rho
+  t <- eval a rho
   check rho gma e t
-  gma1 <- upG gma p t (eval e rho)
+  evalERho <- eval e rho
+  gma1 <- upG gma p t evalERho
   return gma1
-           
+
 checkD rho gma d@(Drec p a e) = do
   debug $ "checking "++ printTree d
   checkT rho gma a
-  let t   = eval a rho
-      gen = genV rho
+  t <- eval a rho
+  let gen = genV rho
   gma1 <- upG gma p t gen
   check (UpVar rho p gen) gma1 e t
-  let v = eval e (UpDec rho d)
+  v <- eval e (UpDec rho d)
   gma2 <- upG gma p t v
   return gma2
 
 checkT rho gma e0 =
   case e0 of
-    EPi  p a b -> do checkT rho gma a
-                     gma1 <- upG gma p (eval a rho) (genV rho)
-                     checkT (UpVar rho p (genV rho)) gma1 b
+    EPi  p a b ->
+      do
+        checkT rho gma a
+        aEval <- (eval a rho)
+        gma1 <- upG gma p aEval (genV rho)
+        checkT (UpVar rho p (genV rho)) gma1 b
     ESig p a b -> checkT rho gma (EPi p a b)
     ESet       -> return ()
     EOne       -> return ()
@@ -322,14 +415,23 @@ check rho gma e0 t0 = do
   case (e0, t0) of
     (ELam p e   , Pi  t g )-> do let gen = genV rho
                                  gma1 <- upG gma p t gen
-                                 check (UpVar rho p gen) gma1 e (g * gen)
+                                 gGen <- g * gen
+                                 check (UpVar rho p gen) gma1 e gGen
     (EPair e1 e2, Sig t g )-> do check rho gma e1 t
-                                 check rho gma e2 (g * eval e1 rho)
+                                 evalE1 <- eval e1 rho
+                                 gEvalE1 <- g * evalE1
+                                 check rho gma e2 gEvalE1
     (ECon c e   , Data _ (cas,rho1))-> do a <- lookupG c cas
-                                          check rho gma e (eval a rho1)
+                                          evalA <- eval a rho1
+                                          check rho gma e evalA
     (ECase _ ces, Pi (Data _ (cas, rho1)) g)
-      | cs == cs1 -> sequence_ [check rho gma e (Pi (eval a rho1) (clCmp g c))
-                               | (Branch c e, (_,a)) <- zip ces cas]
+      | cs == cs1 ->
+        sequence_
+          [do
+            evalA <- eval a rho1
+            check rho gma e (Pi evalA (clCmp g c))
+          | (Branch c e, (_,a)) <- zip ces cas
+          ]
       | otherwise -> fail "case branches does not match the data type"
       where cs  = [c | Branch c _ <- ces]
             cs1 = [c | (c, _)     <- cas]
@@ -337,7 +439,8 @@ check rho gma e0 t0 = do
     (EOne       , Set)-> return ()
     (EPi  p a b , Set)-> do check rho gma a Set
                             let gen = genV rho
-                            gma1 <- upG gma p (eval a rho) gen
+                            evalA <- eval a rho
+                            gma1 <- upG gma p evalA gen
                             check (UpVar rho p gen) gma1 b Set
     (ESig p a b , Set)-> check rho gma (EPi p a b) Set
     (EData _ cas, Set)-> sequence_ [check rho gma a Set | Summand _ a <- cas]
@@ -347,11 +450,13 @@ check rho gma e0 t0 = do
                             eqNf (lRho rho) t t1
   where
   eqNf :: Int -> Nf -> Nf -> G ()
-  eqNf i m1 m2
-    | e1 == e2  = return ()
-    | otherwise = fail $ "eqNf: " ++ printTree e1 ++ "=/=" ++ printTree e2
-    where e1 = rbV i m1
-          e2 = rbV i m2
+  eqNf i m1 m2 =
+    do
+      e1 <- rbV i m1
+      e2 <- rbV i m2
+      if e1 == e2
+        then return ()
+        else fail $ "eqNf: " ++ printTree e1 ++ "=/=" ++ printTree e2
 
 checkI rho gma e0 =
   case e0 of
@@ -359,11 +464,14 @@ checkI rho gma e0 =
     EApp e1 e2 -> do t1 <- checkI rho gma e1
                      (t, g) <- extPiG t1
                      check rho gma e2 t
-                     return (g * eval e2 rho)
+                     evalE2 <- eval e2 rho
+                     g * evalE2
     EFst e     -> fst `fmap` (extSigG =<< checkI rho gma e)
     ESnd e     -> do t <- checkI rho gma e
                      (_, g) <- extSigG t
-                     return (g * vfst (eval e rho))
+                     evalE <- eval e rho
+                     vfstE <- vfst evalE
+                     g * vfstE
 
     e          -> fail ("checkI: " ++ printTree e)
   where
